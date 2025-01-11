941bdc5110853d969632b9aa03ca9703a408566e
Johnny Kessler <68814636+jhKessler@users.noreply.github.com>
2025-01-11 19:19:26 +0100

    fix redirect

--- src/templates/redirect.template.tsx
+++ src/templates/redirect.template.tsx
@@ -6,12 +6,9 @@ export const revalidate = false
 
 export default async function BlogRedirectPage() {
     const availableLanguages = await getProjectLanguages()
-    let defaultLanguage;
-    try {
+    let defaultLanguage = "en";
+    if (availableLanguages[0]?.params.lang) {
         defaultLanguage = availableLanguages[0].params.lang
-    } catch {
-        console.warn(`No articles found. Generate some articles on https://nxtblog.ai first`)
-        notFound()
     }
     redirect(`__BLOG_PATH__/${defaultLanguage}`)
 }
